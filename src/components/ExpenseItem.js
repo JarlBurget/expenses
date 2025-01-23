80110d5c272f084a7919878243f75c8b64925973
Jarl <jarl-ivor.burget@voco.ee>
2025-01-23 15:22:35 +0200

    ExpenseDate.js stiili lisamine

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,7 +5,9 @@ import './ExpenseItem.css'
 
     return (
         <div className='expense-item'>
+            <div>
             <ExpenseDate date={props.data.date}/>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
