0d34f2608389605efa5b4bf359a9888781e9072e
Jarl <jarl-ivor.burget@voco.ee>
2025-01-24 14:40:03 +0200

    Sündmused ja nende handler-id

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,16 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css' 
+import Card from '../UI/Card'
+import { useState } from 'react'
 
  const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.data.title) 
+
+    const clickHandler = () =>{
+        console.log('Clicked!')
+        setTitle(`Update by click ${title}`)
+        console.log(title)
+    } 
 
     return (
         <div className='expense-item'>
@@ -10,6 +19,7 @@ import './ExpenseItem.css'
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
             </div>
+            <button onClick={clickHandler}>Click Me</button>
         </div>
     )
 } 
