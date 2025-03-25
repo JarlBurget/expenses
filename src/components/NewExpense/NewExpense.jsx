53bdffe085cf3e5b6e6285320ce6d1de29dffa7d
JarlBurget <jarl-ivor.burget@voco.ee>
2025-03-25 09:56:11 +0200

    6. ülesande parandused

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,26 +1,40 @@
-import ExpenseForm from "./ExpenseForm"
-import './NewExpense.css'
-
-
-
-
 import { useState } from 'react'
+import "./NewExpense.css"
+import ExpenseForm from "./ExpenseForm"
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
+
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
-        } 
-        props.onSaveExpenseData(expenseData)
-    } 
+        }
+        props.onAddExpense(expenseData)
+        setIsEditing(false);  // Vorm suletakse pärast andmete lisamist
+    }
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData= {saveExpenseDataHandler} />
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            )}
+            {isEditing && (
+                <ExpenseForm 
+                    onSaveExpenseData={saveExpenseDataHandler} 
+                    onCancel={stopEditingHandler} 
+                />
+            )}
         </div>
-
     )
-} 
+}
 
 export default NewExpense
